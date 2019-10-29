eebbdcff655843799b789993c7bb4b48ab17d3d9
arnaud-vanderschriek <arnaud.vanderschrieck@gmail.com>
2019-10-29 16:43:27 +0100

    early root/index.php

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
-require("");
+
+require("models/index.php");
 
 if (isset($_GET["action"])){
-    if($GET["action"]=='');
+    if($_GET["action"]=='nomDeLaFonction'){
+        nomDeLaFonction();
+    }
 }
