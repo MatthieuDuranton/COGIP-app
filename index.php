f9e967654290201c064bbfac3507de5beb90028d
Nassim Koceir <nassim.koceir@gmail.com>
2019-10-30 09:10:43 +0100

    Router

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 session_start();
 
+require('lib/inc.php');
+
 if(isset($_GET['action']) && !empty($_GET['action']) && is_file('controllers/'.$_GET['action'].'.php')){
     require('controllers/'.$_GET['page'].'.php');
 } else {
