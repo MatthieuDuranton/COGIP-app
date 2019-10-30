7d92b9aa7ec7f37d91f35c617a5bc1c0fd07279c
MatthieuDuranton <matthieu.duranton@hotmail.be>
2019-10-30 14:20:59 +0100

    correction model et view

--- models/index.php
+++ models/index.php
@@ -24,21 +24,22 @@ function connection($username, $password){
         }else{
             NULL;
         };
+        if($username_empty == "" AND $password_empty == "" AND $username_err == ""){
+            $req = $db->prepare('SELECT * FROM user WHERE username = :username');//try to connect to DB thanks to the username
+            $req->execute(array('username' => $username));
+            $res = $req->fetch();
+            if ($password == $res["password"]){//check if password is valid ; to use as long as the password is not secured
+            //if(password_verify($password, $res["password"])){//check if password is valid ; to use once password is secured
+                $_SESSION["username"] = $res["username"];//if true : Link the user's infos to the session
+                $_SESSION['firstname'] = $res["firstname"];
+                $_SESSION['lastname'] = $res["lastname"];
+                $_SESSION['fk_role'] = $res["fk_role"];
 
-        $req = $db->prepare('SELECT * FROM user WHERE username = :username');//try to connect to DB thanks to the username
-        $req->execute(array('username' => $username));
-        $res = $req->fetch();
-        if ($password == $res["password"]){//check if password is valid ; to use as long as the password is not secured
-        //if(password_verify($password, $res["password"])){//check if password is valid ; to use once password is secured
-            $_SESSION["username"] = $res["username"];//if true : Link the user's infos to the session
-            $_SESSION['firstname'] = $res["firstname"];
-            $_SESSION['lastname'] = $res["lastname"];
-            $_SESSION['fk_role'] = $res["fk_role"];
+                header("location:?action=dashboard");//if true : moves user to the dashboard
 
-            header("location:?action=dashboard");//if true : moves user to the dashboard
-
-        }else{
-            $session_err = "Désolé, le nom d'utilisateur et le mot de passe ne correspondent pas";
+            }else{
+                $session_err = "Désolé, le nom d'utilisateur et le mot de passe ne correspondent pas";
+            };
         };
     };
 };
