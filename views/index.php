7eed3de1ace6a88d6fb8b27bcdce8f3ae64f5833
arnaud-vanderschriek <arnaud.vanderschrieck@gmail.com>
2019-10-30 16:25:08 +0100

    modif views

--- views/index.php
+++ views/index.php
@@ -2,7 +2,7 @@
     <div class="row">
         <div class="form-group col-md-6">
             <label for="username">Nom d'utilisateur :</label>
-            <input type="text" class="form-control" name="username" placeholder="ex: Jean" value = <?php echo "$username"?>>
+            <input type="text" class="form-control" name="username" placeholder="ex: Jean" value='<?php echo $username ?>'>
             <?= $username_empty = $username_empty ?? NULL; ?>
             <?= $username_err = $username_err ?? NULL; ?>
         </div>
@@ -10,7 +10,7 @@
     <div class="row">
         <div class="form-group col-md-6">
             <label for="password">Mot de passe :</label>
-            <input type="password" class="form-control" name="password" placeholder="ex: Jean">
+            <input type="password" class="form-control" name="password" placeholder="ex: Jean" value='<?php echo $password ?>'>
             <?= $password_empty = $password_empty ?? NULL; ?>
         </div>
     </div>
