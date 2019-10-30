48f8aed098ae4396848ce3dc53e594c6c1a53d15
Nassim Koceir <nassim.koceir@gmail.com>
2019-10-30 12:24:36 +0100

    router correction

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ require('lib/inc.php');
 
 require('core/header.php');
 if(isset($_GET['action']) && !empty($_GET['action']) && is_file('controllers/'.$_GET['action'].'.php')){
-    require('controllers/'.$_GET['page'].'.php');
+    require('controllers/'.$_GET['action'].'.php');
 } else {
 	require('controllers/index.php');
 }
