4974a63ca4bbd727bac38a0e12bd971110b20490
Nassim Koceir <nassim.koceir@gmail.com>
2019-10-30 14:12:34 +0100

    index controller & router correction

--- controllers/index.php
+++ controllers/index.php
@@ -1,4 +1,6 @@
 <?php
 	require('models/index.php');
 	$title = "Login";
+	require('core/header.php');
 	require('views/index.php');
+	require('core/footer.php');
--- index.php
+++ index.php
@@ -3,10 +3,8 @@ session_start();
 
 require('lib/inc.php');
 
-require('core/header.php');
 if(isset($_GET['action']) && !empty($_GET['action']) && is_file('controllers/'.$_GET['action'].'.php')){
     require('controllers/'.$_GET['action'].'.php');
 } else {
 	require('controllers/index.php');
 }
-require('core/footer.php');
