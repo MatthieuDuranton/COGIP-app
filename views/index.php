08966197486754b48ad1e327b405f7800e9b032a
arnaud-vanderschriek <arnaud.vanderschrieck@gmail.com>
2019-10-30 11:32:52 +0100

    views/index correction

--- views/index.php
+++ views/index.php
@@ -1,7 +1,7 @@
 <?php 
 $username_empty = $password_empty = $username_err = $session_err = "";
-$username = $_POST["$username"];
-$password = $_POST["$password"];
+$username = $_POST[$username];
+$password = $_POST[$password];
 connection($username,$password);
 ?>
 <form method="post" action="">
@@ -9,14 +9,14 @@ connection($username,$password);
         <div class="form-group col-md-6">
             <label for="firstname">Username :</label>
             <input type="text" class="form-control" name="username" placeholder="ex: Jean">
-            <?php echo $username_empty ?>
+            <?php echo $username_empty; ?>
         </div>
     </div>
     <div class="row">
         <div class="form-group col-md-6">
             <label for="firstname">Password :</label>
             <input type="text" class="form-control" name="password" placeholder="ex: Jean">
-            <?php echo $password_empty ?>
+            <?php echo $password_empty; ?>
         </div>
     </div>
     <div class="form-group text-center">
