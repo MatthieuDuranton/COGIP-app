80ceb46c1f10b99fcc28c8f2b9de75b6b8a61df8
MatthieuDuranton <matthieu.duranton@hotmail.be>
2019-10-30 16:56:35 +0100

    mise en place view dashboard

---
+++ views/dashboard.php
@@ -0,0 +1,93 @@
+<div class="row">
+    <div class="form-group col-md-6">
+        <h1>Bienvenue <?= $user_Fname?> <?= $user_Lname?>, vous êtes connecté en tant que <?= $rights?></h1>
+    </div>
+</div>
+<div class = "row text-center mt-4">
+<div class="col-12 col-md-4">
+    <div class = text-center>
+        <h4>Derniers contacts enregistrés</h4>
+    </div>
+    <table>
+        <tr>
+            <th>Prénom</th>
+            <th>Nom de famille</th>
+            <th>Email</th>
+            <th>Société(s)</th>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+    </table>
+</div>
+<div class = "row text-center mt-4">
+<div class="col-12 col-md-4">
+    <div class = text-center>
+        <h4>Dernières factures enregistrées</h4>
+    </div>
+    <table>
+        <tr>
+            <th>Référence</th>
+            <th>Société</th>
+            <th>Date d'émission'</th>
+            <th>Personne de contact</th>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+    </table>
+</div>
