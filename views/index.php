4a59d8e68656557b78ac5937bb443a99f0f36661
Nassim Koceir <nassim.koceir@gmail.com>
2019-10-30 15:18:15 +0100

    isset variable

--- views/index.php
+++ views/index.php
@@ -3,20 +3,20 @@
         <div class="form-group col-md-6">
             <label for="username">Nom d'utilisateur :</label>
             <input type="text" class="form-control" name="username" placeholder="ex: Jean">
-            <?= $username_empty; ?>
-            <?= $username_err; ?>
+            <?= $username_empty = $username_empty ?? NULL; ?>
+            <?= $username_err = $username_err ?? NULL; ?>
         </div>
     </div>
     <div class="row">
         <div class="form-group col-md-6">
             <label for="password">Mot de passe :</label>
             <input type="password" class="form-control" name="password" placeholder="ex: Jean">
-            <?= $password_empty; ?>
+            <?= $password_empty = $password_empty ?? NULL; ?>
         </div>
     </div>
     <div class="row">
         <div class="form-group col-md-6">
-            <?= $session_err; ?>
+            <?= $session_err = $session_err ?? NULL; ?>
         </div>
     </div>
     <div class="form-group text-center">
