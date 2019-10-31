e3fd2aa17feea42c26032166fe7da14005ca3933
MatthieuDuranton <matthieu.duranton@hotmail.be>
2019-10-31 11:20:54 +0100

    évolution du viewer dashboard

--- views/dashboard.php
+++ views/dashboard.php
@@ -3,6 +3,7 @@
         <h1>Bienvenue <?= $user_Fname?> <?= $user_Lname?>, vous êtes connecté en tant que <?= $rights?></h1>
     </div>
 </div>
+
 <div class = "row text-center mt-4">
     <div class="col-12 col-md-4">
         <div class = text-center>
@@ -15,36 +16,7 @@
                 <th>Email</th>
                 <th>Société(s)</th>
             </tr>
-            <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-            </tr>
+            <?=lastContact();?>
         </table>
     </div>
 
@@ -59,36 +31,7 @@
                 <th>Date d'émission'</th>
                 <th>Personne de contact</th>
             </tr>
-            <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-            </tr>
+            
         </table>
     </div>
 
@@ -103,39 +46,11 @@
                 <th>Nationalité</th>
                 <th>Type de relations</th>
             </tr>
-            <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-            </tr>
+            
         </table>
     </div>
 </div>
+
 <div class = "row text-center mt-4">
     <div class="col-6">
         <h4><a href="">Voir tous les fournisseurs</a></h4>
