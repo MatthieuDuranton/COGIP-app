a259054486dc8372c0b20e7a9822516e75eb1410
MatthieuDuranton <matthieu.duranton@hotmail.be>
2019-10-31 10:23:11 +0100

    viewer dashboard fait. Retirer peut-être les tr et td

--- views/dashboard.php
+++ views/dashboard.php
@@ -4,90 +4,144 @@
     </div>
 </div>
 <div class = "row text-center mt-4">
-<div class="col-12 col-md-4">
-    <div class = text-center>
-        <h4>Derniers contacts enregistrés</h4>
+    <div class="col-12 col-md-4">
+        <div class = text-center>
+            <h4><a href="">Voir tous les contacts</a></h4>
+        </div>
+        <table>
+            <tr>
+                <th>Prénom</th>
+                <th>Nom de famille</th>
+                <th>Email</th>
+                <th>Société(s)</th>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </table>
     </div>
-    <table>
-        <tr>
-            <th>Prénom</th>
-            <th>Nom de famille</th>
-            <th>Email</th>
-            <th>Société(s)</th>
-        </tr>
-        <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-        </tr>
-        <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-        </tr>
-        <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-        </tr>
-        <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-        </tr>
-        <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-        </tr>
-    </table>
+
+    <div class="col-12 col-md-4">
+        <div class = text-center>
+            <h4><a href="">Voir toutes les factures</a></h4>
+        </div>
+        <table>
+            <tr>
+                <th>Référence</th>
+                <th>Société</th>
+                <th>Date d'émission'</th>
+                <th>Personne de contact</th>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </table>
+    </div>
+
+    <div class="col-12 col-md-4">
+        <div class = text-center>
+            <h4><a href="">Voir toutes les sociétés</a></h4>
+        </div>
+        <table>
+            <tr>
+                <th>Nom</th>
+                <th>N° de TVA</th>
+                <th>Nationalité</th>
+                <th>Type de relations</th>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </table>
+    </div>
 </div>
 <div class = "row text-center mt-4">
-<div class="col-12 col-md-4">
-    <div class = text-center>
-        <h4>Dernières factures enregistrées</h4>
+    <div class="col-6">
+        <h4><a href="">Voir tous les fournisseurs</a></h4>
     </div>
-    <table>
-        <tr>
-            <th>Référence</th>
-            <th>Société</th>
-            <th>Date d'émission'</th>
-            <th>Personne de contact</th>
-        </tr>
-        <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-        </tr>
-        <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-        </tr>
-        <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-        </tr>
-        <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-        </tr>
-        <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-        </tr>
-    </table>
+    <div class="col-6">
+        <h4><a href="">Voir tous les clients</a></h4>
+    </div>
 </div>
+
