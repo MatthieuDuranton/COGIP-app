95b8ff733e385d95cce5d291d5f6d9742905ddec
Nassim Koceir <54060387+nassimkoceir@users.noreply.github.com>
2019-11-04 21:24:16 +0100

    redo identation

--- controllers/logout.php
+++ controllers/logout.php
@@ -1,3 +1,3 @@
 <?php
 	require('models/logout.php');
-    logout();
+	logout();
--- core/functions.php
+++ core/functions.php
@@ -1,22 +1,21 @@
 <?php
 	function welcome(){
-	    global $user_Fname, $user_Lname, $user_rights, $rights;
+		global $user_Fname, $user_Lname, $user_rights, $rights;
 
-	    $user_Fname = $_SESSION['firstname'];
-	    $user_Lname = $_SESSION['lastname'];
-	    $user_rights = $_SESSION['fk_role'];
+		$user_Fname = $_SESSION['firstname'];
+		$user_Lname = $_SESSION['lastname'];
+		$user_rights = $_SESSION['fk_role'];
 
 		if($user_rights == 1){
 			echo "Bienvenue Grand Maître ".$user_Fname." ".$user_Lname;
 		} else {
 			echo "Bienvenue ".$user_Fname." ".$user_Lname;
 		}
-
 	}
 
 	function fkSelect($table, $foreignKeyID, $column){
 		global $db;
-	    $query = $db->query('SELECT * FROM '.$table.' WHERE id_'.$table.' = '.$foreignKeyID);
+		$query = $db->query('SELECT * FROM '.$table.' WHERE id_'.$table.' = '.$foreignKeyID);
 		$result = $query->fetch();
 
 		$column = explode(", ", $column);
@@ -27,7 +26,7 @@
 
 	function fkOption($table, $column){
 		global $db;
-	    $query = $db->query('SELECT * FROM '.$table.' ORDER BY id_'.$table);
+	  $query = $db->query('SELECT * FROM '.$table.' ORDER BY id_'.$table);
 
 		if($table == "people"){
 			$column = explode(", ", $column);
