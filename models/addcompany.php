4d27fae7408194c78fd2f00764564f48d6461d66
arnaud-vanderschriek <arnaud.vanderschrieck@gmail.com>
2019-11-04 16:19:31 +0100

    corr addcompagny

--- models/addcompany.php
+++ models/addcompany.php
@@ -1,84 +1,45 @@
 <?php
 
-
-
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-   
-        if (isset($_POST["name_company"]) AND empty($_POST["name_company"])){
-            
-            $notfill_Err = 'Il faut renseigner une compagnie !';
-        
-        }
-        else{
-        
-            $name_compagny = htmlspecialchars($_POST["name_compagny"]);
+    $name_company = $tva = $nationalité = $role = "";
+    $notfill_Err ="";
+    global $db;
     
-            if(!preg_match("#[a-zA-Z]#",$name_compagny)){ 
-                
-                $notfill_Err = 'la company n\'est pas valide !';
-              
-                
-            }
-         }
-        // tva
 
-        if(isset($_POST["tva"]) AND empty($_POST["tva"])){
+    
 
-            $notfill_Err = 'Il faut renseigner un numéro de tva!';            
+        if ($_SERVER['REQUEST_METHOD']=="POST"){
+            if (empty($name_company = htmlspecialchars(strip_tags($_POST['name_company'])))) {
+                $notfill_Err = "Merci de remplir ce champ";
+            };
+            if (empty($tva = htmlspecialchars(strip_tags($_POST['tva'])))) {
+                $notfill_Err = "Merci de remplir ce champ";
+            };
+            if (empty($nationalité = htmlspecialchars(strip_tags($_POST['nationalité'])))) {
+                $email_Err = "Merci d'indiquer une nationalité valide";
+            };
+            if (empty($role = htmlspecialchars(strip_tags($_POST['role'])))) {
+                $notfill_Err = "Merci de remplir ce champ";
+            };
         
-        }
-        else{
-            $name_compagny = htmlspecialchars($_POST["tva"]);
-
-            if(!preg_match("",$tva)){
-               
-                $notfill_Err = 'le numéro de tva n\'est pas valide !';
-            }
-        }
-
-        // nationality
-
-        if(isset($_POST["nationalité"]) AND empty($_POST["nationalité"])){
-
-            $notfill_Err = 'Il faut renseigner une nationalité !';            
-        
-        }
-        else{
-            $name_compagny = htmlspecialchars($_POST["nationalité"]);
-
-            if(!preg_match("",$nationalité)){
-               
-                $notfill_Err = 'la nationalité n\'est pas valide !';
-            }
-        }
-
-        // Role
-
-        if(isset($_POST["role"]) AND empty($_POST["role"])){
-
-            $notfill_Err = 'Il faut renseigner un role!';            
-        
-        }
-        else{
-            $name_compagny = htmlspecialchars($_POST["role"]);
-
-            if(!preg_match("",$role)){
-               
-                $notfill_Err = 'le role n\'est pas valide !';
-            }
-        }
        
-        $req = $bd-> exec("INSERT INTO company(company_name, vat, fk_country, fk_type) VALUES(:nomDeCompagnie, :numTva, :nationalité, :numRole");
-        $req -> execute(array(
+        if ($notfill_Err == ""){
+        $req = $db->prepare("INSERT INTO company(company_name, vat, fk_country, fk_type) 
+        VALUES(:company_name, :vat, :fk_country, :fk_type)");
+        $req->execute(array(
 
-            'nomDeCompagnie' => $$name_compagny,
-            'numTva' => $$tva,
-            'nationalité' => $nationalité,
-            'numRole' => $role
+            'company_name' => $name_company,
+            'vat' => $tva,
+            'fk_country' => $nationalité,
+            'fk_type' => $role,
         ));
 
-        echo 'L\'ajout a bien été rajouté !';
+         $send_success ="La compagnie a été ajoutée."; 
+         $name_company = $tva = $nationalité = $role = "";
 
-    }
+    };
 
+};
+
+        
+
+        
