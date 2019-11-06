840d7f84641a535474eeb277835077c90fbd32a0
Nassim Koceir <nassim.koceir@gmail.com>
2019-11-06 16:33:40 +0100

    navbar

--- core/header.php
+++ core/header.php
@@ -17,6 +17,55 @@
 			<li class="nav-item">
 				<a class="nav-link" href="?action=dashboard">Tableau de bord</a>
 			</li>
+
+			<li class="nav-item dropdown">
+				<a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+					Factures
+				</a>
+				<div class="dropdown-menu">
+					<a class="dropdown-item" href="?action=invoices">Liste</a>
+					<?php
+						if($role == 1){
+					?>
+						<a class="dropdown-item" href="?action=addinvoice">Ajouter</a>
+					<?php
+						}
+					?>
+				</div>
+			</li>
+
+			<li class="nav-item dropdown">
+				<a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+					Contacts
+				</a>
+				<div class="dropdown-menu">
+					<a class="dropdown-item" href="?action=people">Liste</a>
+					<?php
+						if($role == 1){
+					?>
+						<a class="dropdown-item" href="?action=addpeople">Ajouter</a>
+					<?php
+						}
+					?>
+				</div>
+			</li>
+
+			<li class="nav-item dropdown">
+				<a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+					Sociétés
+				</a>
+				<div class="dropdown-menu">
+					<a class="dropdown-item" href="?action=companies">Liste</a>
+					<?php
+						if($role == 1){
+					?>
+						<a class="dropdown-item" href="?action=addcompany">Ajouter</a>
+					<?php
+						}
+					?>
+				</div>
+			</li>
+
 			<li class="nav-item">
 				<a class="nav-link" href="?action=logout">Déconnexion</a>
 			</li>
