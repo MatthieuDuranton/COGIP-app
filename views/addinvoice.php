2dd1d0b33f1b7766195a4b68df89815f3a4552e5
Nassim Koceir <nassim.koceir@gmail.com>
2019-11-07 00:35:36 +0100

    refactoring forms

--- views/addinvoice.php
+++ views/addinvoice.php
@@ -1,47 +1,46 @@
-<form method="POST" action=''>
     <div class = "row text-center mt-4">
         <div class="col-12">
             <div class = text-center>
                 <h4>Ajouter une facture</h4>
             </div>
-            <table class="table">
-                <tr>
-                    <th>Référence</th>
-                    <th>Société</th>
-                    <th>Date d'émission</th>
-                    <th>Personne de contact</th>
-                </tr>
-                <tr>
-                    <th>
-                        <input class="form-control" type="text" name="reference" value = <?php echo "$ref"?>>
-                        <span> <?php echo $ref_Err;?></span>
-                    </th>
-                    <th>
-                        <select class="form-control" name="company">
-                        <?php fkOption('company', 'id_company, company_name');?>
-                        </select>
-                        <span> <?php echo $company_Err;?></span>
-                    </th>
-                    <th>
-                        <input class="form-control" type="date" name="emissiondate" value = <?php echo "$date"?>>
-                        <span> <?php echo $date_Err;?></span>
-                    </th>
-                    <th>
-                        <select class="form-control" name="contact"?>
-                        <?php fkOption('people', 'id_people, firstname, lastname');?>
-                        </select>
-                        <span> <?php echo $contact_Err;?></span>
-                    </th>
-                </tr>
-            </table>
+
+            <?php feedback($send_success); ?>
+
+            <form method="POST" action="">
+              <div class="row">
+                <div class="form-group col-3">
+                  <label for="reference">Référence</label>
+                  <input class="form-control" type="text" name="reference" placeholder="Référence" value="<?php echo $ref; ?>">
+                  <span> <?php echo $ref_Err;?></span>
+                </div>
+
+                <div class="form-group col-3">
+                  <label for="company">Société</label>
+                  <select class="form-control" name="company">
+                    <?php fkOption('company', 'id_company, company_name');?>
+                  </select>
+                  <span> <?php echo $company_Err;?></span>
+                </div>
+
+                <div class="form-group col-3">
+                  <label for="emissiondate">Date d'émission</label>
+                  <input class="form-control" type="date" name="emissiondate" placeholder="Date d'émission" value="<?php echo $date; ?>">
+                  <span> <?php echo $date_Err;?></span>
+                </div>
+
+                <div class="form-group col-3">
+                  <label for="contact">Personne de contact</label>
+                  <select class="form-control" name="contact">
+                    <?php fkOption('people', 'id_people, firstname, lastname');?>
+                  </select>
+                  <span> <?php echo $contact_Err;?></span>
+                </div>
+
+                <div class="form-group col-12">
+                  <input type="submit" value="Envoyer" class="btn btn-primary btn-lg">
+                </div>
+              </div>
+            </form>
         </div>
-    </div>
-
-    <div class="col-12 text-center mt-2">
-        <p><?= $send_success; ?></p>
-    </div>
-
-    <div class = "col-12 text-center mt-2">
-        <input type="submit" value="submit">
     </div>
 </form>
--- views/addpeople.php
+++ views/addpeople.php
@@ -1,45 +1,43 @@
-<form method="POST" action=''>
     <div class = "row text-center mt-4">
         <div class="col-12">
             <div class = text-center>
                 <h4>Ajouter un contact</h4>
             </div>
-            <table class="table">
-                <tr>
-                    <th>Prénom</th>
-                    <th>Nom de famille</th>
-                    <th>Email</th>
-                    <th>Société</th>
-                </tr>
-                <tr>
-                    <th>
-                        <input class="form-control" type="text" name="fname" value = <?php echo $fname?>>
-                        <span> <?php echo $fname_Err;?></span>
-                    </th>
-                    <th>
-                        <input class="form-control" type="text" name="lname" value = <?php echo $lname?>>
-                        <span> <?php echo $lname_Err;?></span>
-                    </th>
-                    <th>
-                        <input class="form-control" type="email" name="email" value = <?php echo $email?>>
-                        <span> <?php echo $email_Err;?></span>
-                    </th>
-                    <th>
-                        <select class="form-control" name="company">
-                        	<?php fkOption('company','id_company, company_name'); ?>
-                        </select>
-                        <span> <?php echo $company_Err;?></span>
-                    </th>
-                </tr>
-            </table>
+
+            <?php feedback($send_success); ?>
+
+            <form method="POST" action="">
+              <div class="row">
+                <div class="form-group col-3">
+                  <label for="fname">Prénom</label>
+                  <input class="form-control" type="text" name="fname" placeholder="Prénom" value="<?php echo $fname; ?>">
+                  <span> <?php echo $fname_Err;?></span>
+                </div>
+
+                <div class="form-group col-3">
+                  <label for="lname">Nom de famille</label>
+                  <input class="form-control" type="text" name="lname" placeholder="Nom de famille" value="<?php echo $lname; ?>">
+                  <span> <?php echo $lname_Err;?></span>
+                </div>
+
+                <div class="form-group col-3">
+                  <label for="email">E-mail</label>
+                  <input class="form-control" type="text" name="email" placeholder="E-mail" value="<?php echo $email; ?>">
+                  <span> <?php echo $email_Err;?></span>
+                </div>
+
+                <div class="form-group col-3">
+                  <label for="company">Société</label>
+                  <select class="form-control" name="company">
+                    <?php fkOption('company','id_company, company_name'); ?>
+                  </select>
+                  <span> <?php echo $company_Err;?></span>
+                </div>
+
+                <div class="form-group col-12">
+                  <input type="submit" value="Envoyer" class="btn btn-primary btn-lg">
+                </div>
+              </div>
+            </form>
         </div>
     </div>
-
-    <div class="col-12 text-center mt-2">
-        <p><?= $send_success; ?></p>
-    </div>
-
-    <div class = "col-12 text-center mt-2">
-        <input type="submit" value="submit">
-    </div>
-</form>
